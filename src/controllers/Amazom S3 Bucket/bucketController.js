201688af32db91f7af682ace14b92ab87ee96281
Akhilesh Patil <akhileshpatil12168@gmail.com>
2023-06-23 23:13:54 +0530

    done some formating

--- src/controllers/Amazom S3 Bucket/bucketController.js
+++ src/controllers/Amazom S3 Bucket/bucketController.js
@@ -2,27 +2,27 @@ const S3 = require("aws-sdk/clients/s3");
 require("dotenv").config();
 
 const s3 = new S3({
-  accessKeyId: process.env.accessKeyId,
-  secretAccessKey: process.env.secretAccessKey,
-  region: process.env.region,
+    accessKeyId: process.env.accessKeyId,
+    secretAccessKey: process.env.secretAccessKey,
+    region: process.env.region,
 });
 
-let uploadFile = async (file,path) => {
-  return new Promise(function (resolve, reject) {
-    const uploadParams = {
-      Bucket: process.env.buketName,
-      Key: path + file.originalname,
-      Body: file.buffer,
-    };
+let uploadFile = (file, path) => {
+    return new Promise(function (resolve, reject) {
+        const uploadParams = {
+            Bucket: process.env.buketName,
+            Key: path + file.originalname,
+            Body: file.buffer,
+        };
 
-    s3.upload(uploadParams, function (err, data) {
-      if (err) {
-        return reject({ error: err });
-      }
+        s3.upload(uploadParams, function (err, data) {
+            if (err) {
+                return reject({ error: err });
+            }
 
-      return resolve(data.Location);
+            return resolve(data.Location);
+        });
     });
-  });
 };
 
-module.exports = uploadFile 
+module.exports = uploadFile;
