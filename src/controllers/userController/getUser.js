9b69342c589dfab566fb36aaca3b3fb2683771a3
Akhilesh Patil <akhileshpatil12168@gmail.com>
2023-06-23 23:12:25 +0530

    added encryption for token userId with some formating

--- src/controllers/userController/getUser.js
+++ src/controllers/userController/getUser.js
@@ -1,38 +1,38 @@
+const bcrypt = require("bcrypt");
+
 const userModel = require("../../models/userModel");
 const { isValidObjectId } = require("../../utils/validators");
 
 const getUserDetails = async function (req, res) {
-  try {
-    const userId = req.params.userId;
-    const decodedToken = req.verifyed;
+    try {
+        const userId = req.params.userId;
+        const decodedToken = req.verifyed;
 
-    if (!userId)
-      return res
-        .status(400)
-        .send({ status: false, message: "Please provide userId." });
+        if (!userId)
+            return res.status(400).send({ status: false, message: "Please provide userId." });
 
-    if (!isValidObjectId(userId))
-      return res
-        .status(400)
-        .send({ status: false, message: "Please provide a valid userId." });
+        if (!isValidObjectId(userId))
+            return res
+                .status(400)
+                .send({ status: false, message: "Please provide a valid userId." });
 
-    if (userId !== decodedToken.userId)
-      return res
-        .status(403)
-        .send({ status: false, message: "please login again" });
+        let isCorrectUser = await bcrypt.compare(userId, decodedToken.userId);
 
-    const userData = await userModel.findById(userId);
-    if (!userData)
-      return res
-        .status(404)
-        .send({ status: false, message: "user not found." });
+        if (!isCorrectUser)
+            return res.status(403).send({ status: false, message: "please login again" });
 
-    return res
-      .status(200)
-      .send({ status: true, message: "User profile details", data: userData });
-  } catch (err) {
-    res.status(500).send({ status: false, message: err.message });
-  }
+        const userData = await userModel
+            .findById(userId)
+            .select({ __v: 0, updatedAt: 0, createdAt: 0, isDeleted: 0 })
+            .lean();
+        if (!userData) return res.status(404).send({ status: false, message: "user not found." });
+
+        return res
+            .status(200)
+            .send({ status: true, message: "User profile details", data: userData });
+    } catch (err) {
+        res.status(500).send({ status: false, message: err.message });
+    }
 };
 
 module.exports = getUserDetails;
--- src/controllers/userController/userLogin.js
+++ src/controllers/userController/userLogin.js
@@ -4,54 +4,43 @@ const bcrypt = require("bcrypt");
 const { emptyBody, isValidEmail } = require("../../utils/validators");
 
 const loginUser = async function (req, res) {
-  try {
-    if (emptyBody(req.body))
-      return res
-        .status(400)
-        .send({ status: false, msg: "Email and Password is Requierd" });
+    try {
+        if (emptyBody(req.body))
+            return res.status(400).send({ status: false, msg: "Email and Password is Requierd" });
 
-    const { email, password } = req.body;
+        const { email, password } = req.body;
 
-    if (!email)
-      return res
-        .status(400)
-        .send({ status: false, msg: "User Email is Requierd" });
+        if (!email) return res.status(400).send({ status: false, msg: "User Email is Requierd" });
 
-    if (!password)
-      return res
-        .status(400)
-        .send({ status: false, msg: "User Password is Requierd" });
+        if (!password)
+            return res.status(400).send({ status: false, msg: "User Password is Requierd" });
 
-    if (!isValidEmail(email))
-      return res
-        .status(400)
-        .send({ status: false, msg: "Enter Valid Email Id" });
+        if (!isValidEmail(email))
+            return res.status(400).send({ status: false, msg: "Enter Valid Email Id" });
 
-    let user = await userModel.findOne({ email });
-    if (!user)
-      return res.status(400).send({ status: false, msg: "User not Exist" });
+        let user = await userModel.findOne({ email }).select({ _id: 1, password: 1 }).lean();
+        if (!user) return res.status(400).send({ status: false, msg: "User not Exist" });
 
-    let actualPassword = await bcrypt.compare(password, user.password);
+        let actualPassword = await bcrypt.compare(password, user.password);
 
-    if (!actualPassword)
-      return res
-        .status(400)
-        .send({ status: false, msg: "Incorrect email or password" });
+        if (!actualPassword)
+            return res.status(400).send({ status: false, msg: "Incorrect email or password" });
 
-    let token = jwt.sign({ userId: user._id }, process.env.TOKEN_KEY, {
-      expiresIn: "2d",
-    });
-    res.setHeader("Content-Type", "application/json");
+        const userId = await bcrypt.hash(user["_id"].toString(), 10);
+        let token = jwt.sign({ userId: userId }, process.env.TOKEN_KEY, {
+            expiresIn: "1d",
+        });
+        res.setHeader("Content-Type", "application/json");
 
-    res.set("Authorization", `Bearer ${token}`);
+        res.set("Authorization", `Bearer ${token}`);
 
-    return res.status(200).send({
-      status: true,
-      message: "User login successfully",
-      data: { userId: user._id, token: token },
-    });
-  } catch (err) {
-    return res.status(500).send({ status: false, msg: err.message });
-  }
+        return res.status(200).send({
+            status: true,
+            message: "User login successfully",
+            data: { userId: user._id, token: token },
+        });
+    } catch (err) {
+        return res.status(500).send({ status: false, msg: err.message });
+    }
 };
 module.exports = loginUser;
