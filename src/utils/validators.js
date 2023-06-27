1649b4bdded327cfd74e8c9818afdbec56af3176
Akhilesh Patil <akhileshpatil12168@gmail.com>
2023-06-27 22:20:13 +0530

    new function with formating

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,63 +1,80 @@
 const mongoose = require("mongoose");
 
-const emptyBody = value => {
-    return Object.keys(value).length === 0
-}
+const emptyBody = (value) => {
+    return Object.keys(value).length === 0;
+};
 
-const validTrim = value => {
-    return value.trim().toLowerCase()
-}
+const validTrim = (value) => {
+    return value.trim().toLowerCase();
+};
 
-const isNotProvided = value => {
-    return value.trim() !== ""
-}
-
+const isNotProvided = (value) => {
+    return value.trim() !== "";
+};
 
 const isValidWord = function (value) {
-    return /^[a-zA-Z ]{2,20}$/.test(value)
-}
-
+    return /^[a-zA-Z ]{2,20}$/.test(value);
+};
 
 const isValidEmail = (value) => {
-    return /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(value)
-}
+    return /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(value);
+};
 
 const isValidPhone = (value) => {
-    return /^[6-9]\d{9}$/.test(value)
-}
+    return /^[6-9]\d{9}$/.test(value);
+};
 
 const isValidPwd = (value) => {
-    return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/.test(value)
-
-}
+    return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/.test(value);
+};
 
 const isValidPincode = (value) => {
-    return /^[1-9][0-9]{5}$/.test(value)
-}
+    return /^[1-9][0-9]{5}$/.test(value);
+};
 
 const isValidImage = (value) => {
-    return /^([/|\w|\s|+-.])*\.((jpg)|(png)|(jpeg))$/.test(value)
-}
+    return /^([/|\w|\s|+-.])*\.((jpg)|(png)|(jpeg))$/.test(value);
+};
 
 const isValidObjectId = (value) => {
-    return mongoose.Types.ObjectId.isValid(value)
-}
+    return mongoose.Types.ObjectId.isValid(value);
+};
 
 const isValidString = function (value) {
-    if (value && value == undefined) return false
-    if (typeof value == "string" && value.trim().length == 0) return false
-    return true
+    if (value && value == undefined) return false;
+    if (typeof value == "string" && value.trim().length == 0) return false;
+    return true;
+};
+
+const isValidUserName = (value) => {
+    return /^[a-zA-Z0-9 ]{4,}$/.test(value);
+};
+
+const isValidStatus = (status) => {
+    return ["pending", "completed", "cancelled"].indexOf(status) !== -1;
+};
+function getToken(headers) {
+    for (let header of headers) {
+        if (header.includes("Bearer")) {
+            return header.split(" ")[1];
+        }
+    }
+    return "";
 }
 
-const isValidTitle = (value) => {
-    return /^[a-zA-Z0-9 ]{5,}$/.test(value)
-}
-
-
-
-const isValidStatus = function (status) {
-    return ['pending', 'completed', 'cancelled'].indexOf(status) !== -1
-}
-
-
-module.exports = { emptyBody, isNotProvided, validTrim, isValidWord, isValidEmail, isValidPhone, isValidPwd, isValidPincode, isValidImage, isValidObjectId, isValidString,  isValidTitle, isValidStatus }
+module.exports = {
+    emptyBody,
+    isNotProvided,
+    validTrim,
+    isValidWord,
+    isValidEmail,
+    isValidPhone,
+    isValidPwd,
+    isValidPincode,
+    isValidImage,
+    isValidObjectId,
+    isValidString,
+    isValidUserName,
+    isValidStatus,
+    getToken,
+};
