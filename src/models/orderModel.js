edc7678d572e7aab6bb845a0fcbeefcb9f93f155
Akhilesh Patil <akhileshpatil12168@gmail.com>
2023-06-27 22:22:51 +0530

    major updates with formating

--- src/models/orderModel.js
+++ src/models/orderModel.js
@@ -2,58 +2,109 @@ const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const orderSchema = new mongoose.Schema(
-  {
-    userId: {
-      type: ObjectId,
-      ref: "User",
-      required: true,
-    },
-
-    items: [
-      {
-        productId: {
-          type: ObjectId,
-          ref: "product",
-          required: true,
+    {
+        userId: {
+            type: ObjectId,
+            ref: "User",
+            required: true,
         },
-        quantity: {
-          type: Number,
-          required: true,
+        name: {
+            type: String,
+            required: true,
+            trim: true,
         },
-      },
-    ],
+        phone: {
+          type: String,
+          required: true,
+          trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        address: {
+            shipping: {
+                street: {
+                    type: String,
+                    trim: true,
+                },
+                city: {
+                    type: String,
+                    trim: true,
+                },
+                pincode: {
+                    type: Number,
+                    trim: true,
+                },
+            },
+            billing: {
+                street: {
+                    type: String,
+                    trim: true,
+                },
+                city: {
+                    type: String,
+                    trim: true,
+                },
+                pincode: {
+                    type: Number,
+                    trim: true,
+                },
+            },
+        },
+        items: [
+            {
+                productId: {
+                    type: ObjectId,
+                    ref: "product",
+                },
+                quantity: {
+                    type: Number,
+                },
+                title: { type: String },
+                price: { type: Number },
+                productImage: [],
+            },
+        ],
+        totalPrice: {
+            type: Number,
+        },
+        totalItems: {
+            type: Number,
+        },
+        totalQuantity: {
+            type: Number,
+        },
+        isFreeShipping: {
+            type: Boolean,
+            default: false,
+        },
+        orderdedDate : {
+            type : Date
+        },
+        deliveredDate :{
+            type:Date
+        },
+        cancellable: {
+            type: Boolean,
+            default: true,
+        },
+        status: {
+            type: String,
+            default: "pending",
+            enum: ["pending", "completed", "cancelled"],
+        },
 
-    totalPrice: {
-      type: Number,
-      required: true,
+        deletedAt: {
+            type: Date,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
     },
-    totalItems: {
-      type: Number,
-      required: true,
-    },
-    totalQuantity: {
-      type: Number,
-      required: true,
-    },
-    cancellable: {
-      type: Boolean,
-      default: true,
-    },
-    status: {
-      type: String,
-      default: "pending",
-      enum: ["pending", "completed", "cancelled"],
-    },
-
-    deletedAt: {
-      type: Date,
-    },
-    isDeleted: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 module.exports = mongoose.model("Order", orderSchema);
