f85d0020a248f6231727ab19668effac72850d31
Akhilesh Patil <akhileshpatil12168@gmail.com>
2023-07-14 20:04:51 +0530

    new components

--- src/app.js
+++ src/app.js
@@ -1,21 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Outlet  } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 
-import Body from "./components/Body/Body"
-import Orders from "./components/Body/Orders"
-import Cart from "./components/Body/Cart"
-import Product from "./components/Body/getProduct"
-import Login from "./components/forms/Login"
-import Signup from "./components/forms/SignupForm"
-import User from "./components/Body/User"
+import Body from "./components/Body/Body";
+import Orders from "./components/Body/Orders";
+import Cart from "./components/Body/Cart";
+import Product from "./components/Body/getProduct";
+import Login from "./components/forms/Login";
+import Signup from "./components/forms/SignupForm";
+import User from "./components/Body/User";
+import ResetPassword from "./components/Body/ResetPassword";
+import ForgotPassword from "./components/forms/ForgotPassword";
 
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 import ConnectUs from "./components/Body/ConnectUs";
+import CreateOrder from "./components/Body/CreateOrder";
+import Order from "./components/Body/Order";
+import Admin from "./components/Body/Admin";
+import AdminOrdersPage from "./components/Body/AdminOrders";
 
-const AppLayout = () => { 
+
+const AppLayout = () => {
     return (
         <React.Fragment>
             <Header />
@@ -39,7 +46,16 @@ const appRouter = createBrowserRouter([
             { path: "/login", element: <Login /> },
             { path: "/signup", element: <Signup /> },
             { path: "/user/account", element: <User /> },
-            {path: "/connect", element: <ConnectUs /> }
+            { path: "/connect", element: <ConnectUs /> },
+            { path: "/reset/:usertype/:userId/resetpassword/:token", element: <ResetPassword /> },
+            { path: "/request/resetpassword", element: <ForgotPassword /> },
+            { path: "/user/create/order/", element: <CreateOrder /> },
+            { path: "/user/order/:orderId", element: <Order /> },
+
+            {path: "/admin", element:<Admin />},
+            {path : "/admin/orders/" ,element : <AdminOrdersPage/>} 
+        
+
         ],
     },
 ]);
