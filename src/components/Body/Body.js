a497101087acfb5f7b5c2a9895548feff8d8c056
Akhilesh Patil <akhileshpatil12168@gmail.com>
2023-08-05 02:04:24 +0530

    path change

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import ShimmerBody from "./ShimmerCard";
+import ShimmerBody from "../Shimmer/ShimmerCard";
 import Cookies from "universal-cookie";
 import Card from "./Card";
 
