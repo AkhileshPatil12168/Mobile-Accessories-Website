12fc58886cae6e590e1bf8c0cbf2f8c4aa55c525
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2023-11-15 03:09:27 +0530

    completed with testings.

--- src/controllers/wishListController/getWishList.js
+++ src/controllers/wishListController/getWishList.js
@@ -2,9 +2,9 @@ const bcrypt = require("bcrypt");
 
 const wishListModel = require("../../models/wishListModel");
 
-const addToWishList = async (req, res) => {
+const getWishList = async (req, res) => {
   try {
-    let { productId, userId } = req.params;
+    let { userId } = req.params;
 
     const decodedToken = req.verifyed;
 
@@ -15,20 +15,23 @@ const addToWishList = async (req, res) => {
     if (!isCorrectUser)
       return res.status(403).send({ status: false, message: "please login again" });
 
-    const product = { productId, addedDate: Date.now() + 19800000 };
-
-    const wishListData = await wishListModel.findOneAndUpdate(
-      { userId },
-      { $push: { items: product } },
-      { upsert: true, new: true }
-    );
+    const wishListData = await wishListModel
+      .findOne({ userId })
+      .populate({
+        path: "items.productId",
+        select: "title price productImage ratings",
+        populate: {
+          path: "ratings",
+          select: "averageRating totalUsersRated",
+        },
+      })
+      .lean();
 
     if (!wishListData)
-      return res.status(404).send({ status: true, message: "something went wrong" });
-    
+      return res.status(400).send({ status: true, data: {}, message: "wish list is empty" });
+
     return res.status(200).send({
       status: true,
-      message: "product added Successfully in the wish list",
       data: wishListData,
     });
   } catch (error) {
@@ -36,4 +39,4 @@ const addToWishList = async (req, res) => {
   }
 };
 
-module.exports = addToWishList;
+module.exports = getWishList;
