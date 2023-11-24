3ee2ea4ee8d0b2b60c607e432b7059d861420941
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2023-11-25 00:32:49 +0530

    seprate navbar as per login

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,22 +3,141 @@ import Title from "./Title";
 import { Link, useNavigate } from "react-router-dom";
 import Cookies from "universal-cookie";
 
+const UserNavBar = ({ cUserId, cToken }) => {
+  return (
+    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <li className="px-2">
+        <Link
+          to="/"
+          className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        >
+          Home
+        </Link>
+      </li>
+      <Link
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        to={cToken && cUserId ? "/orders" : "/login"}
+      >
+        <li className="px-2">Orders</li>
+      </Link>
+
+      <Link
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        to={cToken && cUserId ? "/contactus" : "/login"}
+      >
+        <li className="px-2">Contact Us</li>
+      </Link>
+
+      {/* SVG */}
+      <Link
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        to={cToken && cUserId ? "/cart" : "/login"}
+      >
+        <li className="px-2">
+          <span className="group">
+            <svg
+              className="w-6 h-6 text-gray-800 dark:text-white group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"
+              />
+            </svg>
+          </span>
+        </li>
+      </Link>
+
+      <Link
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        to={cToken && cUserId ? "/user/account" : "/login"}
+      >
+        <li className="px-2">
+          <span className="group">
+            <svg
+              className="w-6 h-6 text-gray-800 dark:text-white group group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 19a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 11 14H9a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 10 19Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </span>
+        </li>
+      </Link>
+    </ul>
+  );
+};
+const AdminNavBar = ({ cToken, cAdminId }) => {
+  return (
+    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <Link
+        className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+        to="/"
+      >
+        <li className="px-2">Home</li>
+      </Link>
+      <Link
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        to={cToken && cAdminId ? "/admin/users" : "/login"}
+      >
+        <li className="px-2">Users</li>
+      </Link>
+      <Link
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        to={cToken && cAdminId ? "/admin/orders" : "/login"}
+      >
+        <li className="px-2">Orders</li>
+      </Link>
+      <Link
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        to={cToken && cAdminId ? "/admin/products/summery" : "/login"}
+      >
+        <li className="px-2">Products</li>
+      </Link>
+      <Link
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        to={cToken && cAdminId ? "/user/account" : "/login"}
+      >
+        <li className="px-2">Your Account</li>
+      </Link>
+    </ul>
+  );
+};
+const VendorNavBar = (cVendorId, cToken) => {
+  return( <></>);
+};
 const Header = () => {
   const cookies = new Cookies();
   const navigate = useNavigate();
   3;
 
-
   const cToken = cookies.get("token");
   const cUserId = cookies.get("User");
   const cAdminId = cookies.get("Admin");
-  console.log(cUserId);
+  const cVendorId = cookies.get("Vendor");
+
   const handleLogout = () => {
     cookies.remove("token", { path: "/", domain: process.env.domain });
     cookies.remove("User", { path: "/", domain: process.env.domain });
     cookies.remove("Admin", { path: "/", domain: process.env.domain });
+    cookies.remove("Vendor", { path: "/", domain: process.env.domain });
     navigate("/");
   };
+  let NavBar;
 
   return (
     <>
@@ -26,117 +145,14 @@ const Header = () => {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <Title />
           {cAdminId ? (
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <Link
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                to="/"
-              >
-                <li className="px-2">Home</li>
-              </Link>
-              <Link
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                to={cToken && cAdminId ? "/admin/users" : "/login"}
-              >
-                <li className="px-2">Users</li>
-              </Link>
-              <Link
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                to={cToken && cAdminId ? "/admin/orders" : "/login"}
-              >
-                <li className="px-2">Orders</li>
-              </Link>
-              <Link
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                to={cToken && cAdminId ? "/admin/products/summery" : "/login"}
-              >
-                <li className="px-2">Products</li>
-              </Link>
-              <Link
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                to={cToken && cAdminId ? "/user/account" : "/login"}
-              >
-                <li className="px-2">Your Account</li>
-              </Link>
-            </ul>
+            <AdminNavBar cAdminId={cAdminId} cToken={cToken} />
+          ) : cVendorId ? (
+            <VendorNavBar />
           ) : (
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li className="px-2">
-                <Link
-                  to="/"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                    
-                >
-                  Home
-                </Link>
-              </li>
-              <Link
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                to={cToken && cUserId ? "/orders" : "/login"}
-              >
-                <li className="px-2">Orders</li>
-              </Link>
-
-              <Link
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                to={cToken && cUserId ? "/contactus" : "/login"}
-              >
-                <li className="px-2">Contact Us</li>
-              </Link>
-
-              {/* SVG */}
-              <Link
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                to={cToken && cUserId ? "/cart" : "/login"}
-              >
-                <li className="px-2">
-                  <span className="group">
-                    <svg
-                      className="w-6 h-6 text-gray-800 dark:text-white group-hover:text-blue-500"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 18 20"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M6 15a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0h8m-8 0-1-4m9 4a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-9-4h10l2-7H3m2 7L3 4m0 0-.792-3H1"
-                      />
-                    </svg>
-                  </span>
-                </li>
-              </Link>
-
-              <Link
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                to={cToken && cUserId ? "/user/account" : "/login"}
-              >
-                <li className="px-2">
-                  <span className="group">
-                    <svg
-                      className="w-6 h-6 text-gray-800 dark:text-white group group-hover:text-blue-500"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 20"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M10 19a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 11 14H9a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 10 19Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                      />
-                    </svg>
-                  </span>
-                </li>
-              </Link>
-            </ul>
+            <UserNavBar cUserId={cUserId} cToken={cToken} />
           )}
 
-          {cToken && (cUserId || cAdminId) ? (
+          {cToken && (cUserId || cAdminId || cVendorId) ? (
             <button
               className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
               onClick={() => {
