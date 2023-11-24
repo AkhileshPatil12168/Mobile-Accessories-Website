4bca78ea8d694a2c3423b762a46939730158c10e
VaibhavAPatil <Vaibhavpatil.vap@gmail.com>
2023-11-24 20:48:01 +0530

    Added Reveiw

--- src/components/Body/getProduct.js
+++ src/components/Body/getProduct.js
@@ -8,9 +8,8 @@ const Product = (props) => {
   const navigate = useNavigate();
   const cookies = new Cookies();
   const cUserId = cookies.get("User");
-  const {ratings} = props;
+  const { ratings } = props;
   const { id } = useParams();
-  
 
   const [product, setProduct] = useState();
   const [text, setText] = useState("Add to cart");
@@ -28,7 +27,7 @@ const Product = (props) => {
         }
       );
       setProduct(response.data.data.productsDetails);
-       console.log(response.data.data.productsDetails)
+      console.log(response.data.data.productsDetails);
     } catch (error) {
       console.error(error);
     }
@@ -165,8 +164,7 @@ const Product = (props) => {
             <button
               onClick={addToCart}
               disabled={res == 400 ? true : false}
-              className={
-              `flex items-center justify-center ${lineThrough} ${cursor} text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-1 w-full`}
+              className={`flex items-center justify-center ${lineThrough} ${cursor} text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-1 w-full`}
             >
               {text}
             </button>
@@ -183,9 +181,63 @@ const Product = (props) => {
           {/* <a href="#" className="inline-block bg-[#ff5722] text-white no-underline rounded text-lg transition-[background-color] duration-[0.3s] px-5 py-2.5 hover:bg-[#f44336]">
                         Add to Cart
                     </a> */}
-
         </div>
       </div>
+
+      {/* Review */}
+      <article className=" mx-auto my-5 p-5 border-gray-300 border-t-2 w-9/12">
+        <div className="flex items-center mb-4">
+          <img
+            className="w-10 h-10 me-4 rounded-full object-cover"
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP._6kSqsTmX5o4yeSjGnw48AHaLH%26pid%3DApi&f=1&ipt=9f1127d1d114e5d2f8dcf666000e632c5af7d399463324f43a6f5b4778666792&ipo=images"
+            alt=""
+          />
+          <div className="font-medium dark:text-white">
+            <p>
+              Jese Leos{" "}
+              <time
+                dateTime="2014-08-16 19:00"
+                className="block text-sm text-gray-500 dark:text-gray-400"
+              >
+                Joined on August 2014
+              </time>
+            </p>
+          </div>
+        </div>
+        <div className="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
+          <p>4</p>
+          <svg
+            className="w-4 h-4 text-yellow-300"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 22 20"
+          >
+            <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+          </svg>
+          <h3 className="ms-2 text-sm font-semibold text-gray-900 dark:text-white">
+            Thinking to buy another one!
+          </h3>
+        </div>
+        <p className="mb-2 text-gray-500 dark:text-gray-400">
+          This is my third Invicta Pro Diver. They are just fantastic value for
+          money. This one arrived yesterday and the first thing I did was set
+          the time, popped on an identical strap from another Invicta and went
+          in the shower with it to test the waterproofing.... No problems.
+        </p>
+        <p className="mb-3 text-gray-500 dark:text-gray-400">
+          It is obviously not the same build quality as those very expensive
+          watches. But that is like comparing a Citroën to a Ferrari. This watch
+          was well under £100! An absolute bargain.
+        </p>
+        <a
+          href="#"
+          className="block mb-5 text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+        >
+          Read more
+        </a>
+
+      </article>
     </div>
   ) : (
     <h1>no data </h1>
