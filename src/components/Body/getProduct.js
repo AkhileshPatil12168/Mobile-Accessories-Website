0398c0db07dc37b666c6c5aea1e4f1ff7ebe8fc2
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2023-11-24 13:22:44 +0530

    added linethough and cursor to the add to cart button

--- src/components/Body/getProduct.js
+++ src/components/Body/getProduct.js
@@ -166,7 +166,7 @@ const Product = (props) => {
               onClick={addToCart}
               disabled={res == 400 ? true : false}
               className={
-              `flex items-center justify-center  text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-1 w-full`}
+              `flex items-center justify-center ${lineThrough} ${cursor} text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-1 w-full`}
             >
               {text}
             </button>
