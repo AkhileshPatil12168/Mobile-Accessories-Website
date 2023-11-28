051e4fbd6e2f9c465367f9d983dcc03d68a57cc0
VaibhavAPatil <Vaibhavpatil.vap@gmail.com>
2023-11-28 21:12:10 +0530

    Changed the Design Of Admin Order Panel

--- src/components/Body/user/Wishlist.js
+++ src/components/Body/user/Wishlist.js
@@ -79,6 +79,7 @@ console.log(item.productId._id)
           </div>
           <div className="flex flex-col mx-auto justify-between my-2">
           
+          
             <button
               onClick={() => handleAddToCart(item?.productId?._id)}
               className={`${color} text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2`}
