5ad61212079e111c21c0e805b792c82b26403300
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2023-11-29 13:29:32 +0530

    minor changes by the responce

--- src/components/Body/vendor/VendorProducts.js
+++ src/components/Body/vendor/VendorProducts.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 import Cookies from "universal-cookie";
 
-import {  useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 const VendorProductsPage = () => {
-  const navigate = useNavigate();
   const cookies = new Cookies();
   const cVendorId = cookies.get("Vendor");
 
-  const [products, setProducts]=useState([])
+  const [products, setProducts] = useState([]);
 
   const getProducts = async () => {
     try {
@@ -18,7 +17,7 @@ const VendorProductsPage = () => {
 
         { withCredentials: true }
       );
-      setProducts(responce?.data?.data)
+      setProducts(responce?.data?.data);
       console.log(responce.data.data);
     } catch (error) {
       console.log(error);
@@ -33,15 +32,24 @@ const VendorProductsPage = () => {
       <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8">
         {products.map((product) => (
           // Single Product
-          <div key={product._id} className="bg-gray-100 p-4 rounded-md border border-gray-100 hover:border-blue-500 hover:rounded hover:border-1 hover:duration-300 hover:ease-in-out">
-            <img
-              src={product.productImage[0]}
-              alt={product.title}
-              className="w-full h-60 object-cover mb-4 rounded-md"
-            />
-            <h3 className="text-lg font-bold mb-2">{product.title}</h3>
-            <p className="text-green-500 mb-2 font-medium">₹{product.price}</p>
-            <p className="text-gray-700 mb-2 font-medium">Available Quantity: <span className="text-violet-500">{product.available_Quantity}</span></p>
+          <div
+            key={product._id}
+            className="bg-gray-100 p-4 rounded-md border border-gray-100 hover:border-blue-500 hover:rounded hover:border-1 hover:duration-300 hover:ease-in-out"
+          >
+            <Link to={"/vendor/product/" + product?._id}>
+              <img
+                src={product?.productImage[0]}
+                alt={product?.title}
+                className="w-full h-60 object-cover mb-4 rounded-md"
+              />
+            </Link>
+            <h3 className="text-lg font-bold mb-2">{product?.title}</h3>
+            <p >ProductId: {product?._id}</p>
+            <p className="text-green-500 mb-2 font-medium">₹{product?.price}</p>
+            <p className="text-gray-700 mb-2 font-medium">
+              Available Quantity:{" "}
+              <span className="text-violet-500">{product?.available_Quantity}</span>
+            </p>
           </div>
         ))}
       </div>
