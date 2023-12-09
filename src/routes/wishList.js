434c5ce1481a4e37319fbbc0d89678d69c7bc5ab
AkhileshPatil12168 <akhileshpatil12168@gmail.com>
2023-12-09 16:32:43 +0530

    post to put method

--- src/routes/wishList.js
+++ src/routes/wishList.js
@@ -6,7 +6,7 @@ const addToWishList = require("../controllers/wishListController/addToWishList")
 const getWishList = require("../controllers/wishListController/getWishList");
 const deleteItemWishList = require("../controllers/wishListController/removeFromWishList");
 
-Router.post("/user/:userId/product/:productId/wishlist", authentication, addToWishList);
+Router.put("/user/:userId/product/:productId/wishlist", authentication, addToWishList);
 Router.get("/user/:userId/wishlist", authentication, getWishList);
 Router.delete("/user/:userId/wishlist/:wishListId/item/:itemId",authentication,deleteItemWishList);
 
